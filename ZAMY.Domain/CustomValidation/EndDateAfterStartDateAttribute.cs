7f3b7265ca0f5dbac7e4fba45febc4a997dc32a4
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-02-26 10:10:23 -0800

    add custom validation

---
+++ ZAMY.Domain/CustomValidation/EndDateAfterStartDateAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAMY.Domain.CustomValidation
+{
+    public class EndDateAfterStartDateAttribute:ValidationAttribute
+    {
+        private readonly string _startDate;
+        public EndDateAfterStartDateAttribute(string startDate)
+        {
+            _startDate=startDate;
+        }
+        protected override ValidationResult? IsValid(object value,ValidationContext validationContext)
+        {
+            try
+            {
+                
+                var property = validationContext.ObjectType.GetProperty(_startDate);
+
+                var startDate = (DateTime)property.GetValue(validationContext.ObjectInstance);
+
+                var endDate = (DateTime)value;
+
+                if (endDate >= startDate)
+                    return new ValidationResult($"End Date must be after the {_startDate}");
+
+                return ValidationResult.Success;
+            }
+            catch
+            {
+               return new ValidationResult("Wrong!");
+            }
+            
+        }
+    }
+}
