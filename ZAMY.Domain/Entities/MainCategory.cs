5718bb3756ba36b6f06f9ad5202ffdf9465ca2bb
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-02-26 09:50:17 -0800

    make name non-nullable,initial navigation Meal

--- ZAMY.Domain/Entities/MainCategory.cs
+++ ZAMY.Domain/Entities/MainCategory.cs
@@ -2,7 +2,7 @@
 {
     public class MainCategory : _BaseEntity
     {
-        public string Name { get; set; } = null!;
-        public ICollection<Meal?> Meals { get; set; } = new HashSet<Meal?>();
+        public string Name { get; set; } 
+        public ICollection<Meal> Meals { get; set; }=new HashSet<Meal>(); 
     }
 }
