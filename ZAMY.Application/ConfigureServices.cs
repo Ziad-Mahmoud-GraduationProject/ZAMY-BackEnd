3f39777c10ae43ce68b19db70b7721c05abd7e65
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-02-27 12:34:21 -0800

    inject maincategory

--- ZAMY.Application/ConfigureServices.cs
+++ ZAMY.Application/ConfigureServices.cs
@@ -1,10 +1,13 @@
-﻿namespace ZAMY.Application
+﻿using ZAMY.Application.Services.MainCategories;
+
+namespace ZAMY.Application
 {
     public static class ConfigureServices
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IMealService, MealService>();
+            services.AddScoped<IMainCategoryService, MainCategoryService>();
 
             return services;
         }
