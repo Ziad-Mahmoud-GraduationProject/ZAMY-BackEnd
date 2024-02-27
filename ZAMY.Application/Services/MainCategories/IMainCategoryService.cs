2d8df33bb9f6100812145d9f8651f39d8fdb7091
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-02-26 16:45:19 -0800

    Add IMainCategoryService

---
+++ ZAMY.Application/Services/MainCategories/IMainCategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAMY.Application.Services.MainCategories
+{
+    public interface IMainCategoryService
+    {
+        IEnumerable<MainCategory> GetAll();
+         MainCategory GetById(int id);
+        MainCategory GetCategoryName(string maincategoryName);
+        MainCategory Add(MainCategory maincategory);
+        MainCategory Update(MainCategory maincategory);
+        MainCategory Delete(int id);
+    }
+}
