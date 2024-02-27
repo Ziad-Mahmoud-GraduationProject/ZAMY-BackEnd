2b737380f19c5a61afbb8d8767298565926ee2ed
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-02-26 16:49:10 -0800

    Add MainCategoryService

---
+++ ZAMY.Application/Services/MainCategories/MainCategoryService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAMY.Application.Services.MainCategories
+{
+    public class MainCategoryService(IUnitOfWork _uitOfWork) : IMainCategoryService
+    {
+        public IEnumerable<MainCategory> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MainCategory GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MainCategory GetCategoryName(string maincategoryName)
+        {
+            throw new NotImplementedException();
+        }
+        public MainCategory Add(MainCategory maincategory)
+        {
+            throw new NotImplementedException();
+        }
+        public MainCategory Update(MainCategory maincategory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MainCategory Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
