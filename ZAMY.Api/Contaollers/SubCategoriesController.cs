8ac8d4f5309f795a474a32a658dc86b77db61dcd
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-02-28 09:30:47 -0800

    add subcategory controller

---
+++ ZAMY.Api/Contaollers/SubCategoriesController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZAMY.Api.Dtos.subcategories;
+using ZAMY.Application.Services.SubCategories;
+
+namespace ZAMY.Api.Contaollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubCategoriesController(ISubCategoryService _subCategoryService) : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+
+            var maincategories = _subCategoryService.GetAll();
+
+            if (maincategories is null)
+
+                return NotFound("not found any category !");
+
+            return Ok(maincategories);
+        }
+
+        [HttpGet("GetById{id}")]
+        public IActionResult GetById(int id)
+        {
+
+            var subcategories = _subCategoryService.GetById(id);
+
+            if (subcategories is null)
+
+                return NotFound($"not found any category has {id} Id !");
+
+            return Ok(subcategories);
+        }
+        [HttpGet("GetByName{name}")]
+        public IActionResult GetByName(string name)
+        {
+
+            var subcategories = _subCategoryService.GetCategoryName(name);
+
+            if (subcategories is null)
+
+                return NotFound($"not found any category has {name} Name !");
+
+            return Ok(subcategories);
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(SubCategoryDto dto)
+        {
+            var subCategory = new SubCategory
+            {
+                Name = dto.Name
+            };
+            _subCategoryService.Add(subCategory);
+
+            return Ok(subCategory);
+        }
+        [HttpPut("Update{id}")]
+        public IActionResult Update(int id, SubCategoryDto dto)
+        {
+            var subCategory = _subCategoryService.GetById(id);
+
+            if (subCategory is null)
+
+                return NotFound($"not found any category has {id} Id !");
+
+            subCategory.Name = dto.Name;
+
+            _subCategoryService.Update(subCategory);
+
+            return Ok(subCategory);
+        }
+        [HttpDelete("Delete{id}")]
+        public IActionResult Delete(int id)
+        {
+            var subCategory = _subCategoryService.GetById(id);
+
+            if (subCategory is null)
+
+                return NotFound($"not found any category has {id} Id !");
+
+            _subCategoryService.Delete(subCategory);
+
+            return Ok(subCategory);
+
+        }
+    }
+}
