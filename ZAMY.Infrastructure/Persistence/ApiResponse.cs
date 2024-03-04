37f27d6927f7234ebbf29f931c01ea6031190e21
Ziad Mahmoud <ziadma421@outlook.com>
2024-03-04 21:44:47 +0200

    Add Api Response

---
+++ ZAMY.Infrastructure/Persistence/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace ZAMY.Infrastructure.Persistence
+{
+    public class ApiResponse
+    {
+        public bool IsSuccess { get; set; } = true;
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+        public List<string> ErrorMessage { get; set; } = new List<string>();
+        public object Result { get; set; }
+    }
+}
