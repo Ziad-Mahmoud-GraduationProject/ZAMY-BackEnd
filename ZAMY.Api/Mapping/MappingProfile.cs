485a9bee07b32c517868642ee4cabda701d8c9ab
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-03-07 12:30:35 -0800

    add mapping cartitem

--- ZAMY.Api/Mapping/MappingProfile.cs
+++ ZAMY.Api/Mapping/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using ZAMY.Api.Dtos.CartItem.incommig;
+using ZAMY.Api.Dtos.CartItem.outcomming;
 using ZAMY.Api.Dtos.Kitchen.incoming;
 using ZAMY.Api.Dtos.Kitchen.outcoming;
 
@@ -18,6 +20,14 @@ namespace ZAMY.Api.Mapping
         { 
             CreateMap<CreateKitchenDto, Kitchen>();
             CreateMap<Kitchen, KitchenDto>();  
+        }
+    }
+    public class MappingProfileCartItem : Profile
+    {
+        public MappingProfileCartItem()
+        {
+            CreateMap<CreateCartItem, CartItem>();
+            CreateMap<CartItem, CartItemDto>();
         }
     }
 }
