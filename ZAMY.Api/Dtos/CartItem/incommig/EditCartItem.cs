58a580d3e833a372c57c05877fc45e72621c2d03
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-03-07 12:26:23 -0800

    add Dto CartItem

---
+++ ZAMY.Api/Dtos/CartItem/incommig/EditCartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ZAMY.Api.Dtos.CartItem.incommig
+{
+    public class EditCartItem
+    {
+        public int Quantity { get; set; }
+        public int MealId { get; set; }
+        public int CartId { get; set; }
+        public int orderId { get; set; }
+    }
+}
---
+++ ZAMY.Api/Dtos/CartItem/outcomming/CartItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ZAMY.Api.Dtos.CartItem.outcomming
+{
+    public class CartItemDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int MealId { get; set; }
+        public int CartId { get; set; }
+        public int orderId { get; set; }
+    }
+}
