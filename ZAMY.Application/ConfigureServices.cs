dfe8900f9a9482fcfb3ddd5312f3785adc9e777d
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-03-09 01:06:35 -0800

    add DI Order

--- ZAMY.Application/ConfigureServices.cs
+++ ZAMY.Application/ConfigureServices.cs
@@ -2,6 +2,7 @@
 using ZAMY.Application.Services.KitchenPhotos;
 using ZAMY.Application.Services.Kitchens;
 using ZAMY.Application.Services.MainCategories;
+using ZAMY.Application.Services.Orders;
 using ZAMY.Application.Services.SubCategories;
 
 namespace ZAMY.Application
@@ -15,6 +16,7 @@ namespace ZAMY.Application
             services.AddScoped<ISubCategoryService, SubCategoryService>();
             services.AddScoped<IKitchenService, KitchenService>();
             services.AddScoped<ICartItemService, CartItemService>();
+            services.AddScoped<IOrderService, OrderService>();
             return services;
         }
     }
