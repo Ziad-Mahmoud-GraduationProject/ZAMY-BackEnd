55636c4ef812d6a52594bb120ee49b1e3da66783
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-03-09 01:02:47 -0800

    add Order Service

--- ZAMY.Application/Services/Orders/IOrderService.cs
+++ ZAMY.Application/Services/Orders/IOrderService.cs
@@ -1,6 +1,17 @@
-﻿namespace ZAMY.Application.Services.Order
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAMY.Application.Services.Orders
 {
     public interface IOrderService
     {
+        IEnumerable<Order> GetAll();
+        Order GetById(int id);
+        Order Add(Order order);
+        Order Update(Order order);
+        Order Delete(Order order);
     }
 }
--- ZAMY.Application/Services/Orders/OrderService.cs
+++ ZAMY.Application/Services/Orders/OrderService.cs
@@ -1,7 +1,33 @@
-﻿namespace ZAMY.Application.Services.Order
-{
-    public class OrderService(IUnitOfWork unitOfWork) : IOrderService
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ZAMY.Application.Services.Orders
+{
+    internal class OrderService(IUnitOfWork _unitOfWork) : IOrderService
+    {
+        public IEnumerable<Order> GetAll()=>_unitOfWork.Orders.GetAll();
+
+        public Order GetById(int id) => _unitOfWork.Orders.GetById(id);    
+        public Order Add(Order order)
+        {
+            _unitOfWork.Orders.Add(order);
+            _unitOfWork.Complete();
+            return order;
+        }
+        public Order Update(Order order)
+        {
+            _unitOfWork.Orders.Update(order);
+            _unitOfWork.Complete();
+            return order;
+        }
+        public Order Delete(Order order)
+        {
+            _unitOfWork.Orders.Remove(order);
+            _unitOfWork.Complete();
+            return order;
+        } 
     }
 }
