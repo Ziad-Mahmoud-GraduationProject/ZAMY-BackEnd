988d399155238e7d731ca856be0d3ee99636b18a
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-03-09 01:05:03 -0800

    add Order Dto

---
+++ ZAMY.Api/Dtos/Orders/incomming/CreateOrder.cs
@@ -0,0 +1,14 @@
+﻿using ZAMY.Domain.Enums;
+
+namespace ZAMY.Api.Dtos.Orders.incomming
+{
+    public class CreateOrder
+    {
+        public string PaymentMethod { get; set; }
+        public Status Status { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Notes { get; set; }
+        public int CustomerId { get; set; }
+        public int KitchenId { get; set; }
+    }
+}
---
+++ ZAMY.Api/Dtos/Orders/outcomming/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿using ZAMY.Domain.Enums;
+
+namespace ZAMY.Api.Dtos.Orders.outcomming
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public string PaymentMethod { get; set; }
+        public Status Status { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Notes { get; set; }
+        public int CustomerId { get; set; }
+        public int KitchenId { get; set; }
+    }
+}
