25bddf3096d40c782860a9c1b5cc9fda73251174
ziad.mahmoud21.07@hotmail.com <ziad.mahmoud21.07@hotmail.com>
2024-03-28 08:28:12 +0200

    Add Authentication Services

--- ZAMY.Api/Program.cs
+++ ZAMY.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using ZAMY.Api;
 using ZAMY.Api.Mapping;
 using ZAMY.Api.Middlewares;
@@ -22,6 +26,36 @@ builder.Services.AddTransient<GlobalExeptionHandlingMiddleware>();
 //builder.Services.AddAutoMapper(typeof(MappingProfile));
 //builder.Services.AddMappingServices();
 
+
+
+//JWT
+builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
+
+//Authentication
+
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(option =>
+    {
+        option.RequireHttpsMetadata = false;
+        option.SaveToken = true;
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("JWT:Key"))),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,7 +73,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseMiddleware<GlobalExeptionHandlingMiddleware>();
 }
-
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 app.MapControllers();
 
 app.Run();
