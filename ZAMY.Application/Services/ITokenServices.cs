cc0c22d42afba10f23dc9e9e759eee3a256cbefb
ziad.mahmoud21.07@hotmail.com <ziad.mahmoud21.07@hotmail.com>
2024-03-28 08:41:05 +0200

    Add TokenServices

---
+++ ZAMY.Application/Services/ITokenServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAMY.Infrastructure
+{
+    public interface ITokenServices
+    {
+         Task<JwtSecurityToken> GetTokenAsync(IdentityUser user);
+    }
+}
---
+++ ZAMY.Infrastructure/ITokenServices.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAMY.Infrastructure
+{
+    public interface ITokenServices
+    {
+        string GetToken(IdentityUser user);
+    }
+}
