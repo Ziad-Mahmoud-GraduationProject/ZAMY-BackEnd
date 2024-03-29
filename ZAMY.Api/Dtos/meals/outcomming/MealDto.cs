f49660573446a0f2636112eea23e7df3a3bce2d2
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-03-29 11:33:29 -0700

    add Dto

---
+++ ZAMY.Api/Dtos/meals/outcomming/MealDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ZAMY.Api.Dtos.meals.outcomming
+{
+    public class MealDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public double Rating { get; set; }
+        public DateTime PreparationTime { get; set; }
+        public string Ingredients { get; set; }
+        public int KitchenId { get; set; }
+        public int MainCategoryId { get; set; }
+        public int SubCategoryId { get; set; }
+        public int OfferId { get; set; }
+    }
+}
