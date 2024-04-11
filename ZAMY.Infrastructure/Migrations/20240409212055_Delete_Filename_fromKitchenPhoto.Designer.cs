21ee281089916f6976a82e20dd7f6eb81dd56854
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-04-11 03:23:21 -0700

    add migration to update photo

--- ZAMY.Infrastructure/Migrations/20240409212055_Delete_Filename_fromKitchenPhoto.Designer.cs
+++ ZAMY.Infrastructure/Migrations/20240409212055_Delete_Filename_fromKitchenPhoto.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZAMY.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using ZAMY.Infrastructure.Persistence;
 namespace ZAMY.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240409212055_Delete_Filename_fromKitchenPhoto")]
+    partial class Delete_Filename_fromKitchenPhoto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -756,9 +759,8 @@ namespace ZAMY.Infrastructure.Migrations
                     b.Property<int>("KitchenId")
                         .HasColumnType("int");
 
-                    b.Property<string>("Phone")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<int>("Phone")
+                        .HasColumnType("int");
 
                     b.Property<DateTime>("UpdateOn")
                         .HasColumnType("datetime2");
