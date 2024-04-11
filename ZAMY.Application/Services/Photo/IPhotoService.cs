a26e3e1bc29c13b5e7324f91f691e93e2e56eed8
mohamed abdelhamid <mohamed7abdelhamid594@gmail.com>
2024-04-11 03:07:23 -0700

    add photoservice

---
+++ ZAMY.Application/Services/Photo/IPhotoService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZAMY.Domain.Entities;
+
+namespace ZAMY.Application.Services.Photo
+{
+    public interface IPhotoService
+    {
+         Tuple<int, string> SaveImage(IFormFile File);
+         string UploadImage(IFormFile file);
+         bool ImageExtension(IFormFile fileName, string[] allowedExtensions);
+         bool ImageLength(IFormFile fileName,long imageLength);
+        void AddToMeal(MealPhoto mealPhoto);
+        void AddToKitchen(KitchenPhoto kitchenPhoto);
+        bool DeleteImage(string image);
+    }
+}
